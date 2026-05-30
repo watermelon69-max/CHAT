388bd9d1a37dd8630395604e9ad07f88b33c1093
Anurag Mourya <you@example.com>
2026-05-30 20:11:31 +0530

    refactor: update server.js to serve frontend files and improve path handling

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectDB from "./db/connectToMongoDB.js";
-import {app,  server } from "./socket/socket.js";
-
+import { app, server } from "./socket/socket.js";
 
 dotenv.config({ path: "./backend/.env" });
+
+const __dirname = path.resolve();
 
 //middleware to parse incoming body data (from req.body)
 app.use(express.json());
@@ -19,6 +21,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
